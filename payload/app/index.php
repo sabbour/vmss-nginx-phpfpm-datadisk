250aa7adb081586e5b06047e1caa2d8ec4b81e2c
Ahmed Sabbour <sabbour@outlook.com>
2018-05-06 23:28:54 +0400

    Updated provisioner to take build image name

--- payload/app/index.php
+++ payload/app/index.php
@@ -5,7 +5,7 @@
 ?>
 
 <h1>SERVER_ADDR: <?php echo $_SERVER['SERVER_ADDR'] ?>
-<h2>BUILD: <?php $file = file_get_contents('/usr/share/nginx/html/default/buildnumber.txt', FILE_USE_INCLUDE_PATH); echo $file; ?>
+<h2>BUILD: <?php $file = file_get_contents('./buildnumber.txt', FILE_USE_INCLUDE_PATH); echo $file; ?>
 <br/>
 <br/>
 <?php phpinfo() ?>
