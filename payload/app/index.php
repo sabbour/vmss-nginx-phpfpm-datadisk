2e1013da0cbba691356b3ed32d689203c48b0551
Ahmed Sabbour <sabbour@outlook.com>
2018-05-06 21:25:07 +0400

    Updated app to read build number

--- payload/app/index.php
+++ payload/app/index.php
@@ -1,3 +1,5 @@
 <h1>SERVER_ADDR: <?php echo $_SERVER['SERVER_ADDR'] ?>
+<h2>BUILD: <?php $file = file_get_contents('./buildnumber.txt', FILE_USE_INCLUDE_PATH); echo $file; ?>
+<br/>
 <br/>
 <?php phpinfo() ?>
